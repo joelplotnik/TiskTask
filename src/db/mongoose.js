cf657185a4fa8cfc74d550320ed9af83c42fa4bd
Joel Plotnik <joelplotnik@gmail.com>
2022-10-24 15:05:26 -0700

    Implement data validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,18 +1,55 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
 const User = mongoose.model('User', {
-  name: { type: String },
-  age: { type: Number },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number');
+      }
+    },
+  },
 });
 
-const Task = mongoose.model('Task', {
-  description: { type: String },
-  completed: { type: Boolean },
-});
+// CREATE A NEW USER
+// const me = new User({
+//   name: '  Joel ',
+//   email: 'JOELPLOTNIK@gmail.com    ',
+//   password: '1111111',
+//   age: 37,
+// });
 
-// const me = new User({ name: 'Joel', age: 37 });
 // me.save()
 //   .then(() => {
 //     console.log(me);
@@ -21,9 +58,21 @@ const Task = mongoose.model('Task', {
 //     console.log('Error!', error);
 //   });
 
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// CREATE A NEW TASK
 const task = new Task({
-  description: 'Learn the Mongoose library',
-  completed: false,
+  description: 'Eat lunch',
 });
 task
   .save()
