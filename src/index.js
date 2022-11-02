ccc77fb6382a907edcf8f8e0e99ef0f224327531
Joel Plotnik <joelplotnik@gmail.com>
2022-11-02 10:42:03 -0700

    Validate file uploads

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ const port = process.env.PORT || 3000;
 const multer = require('multer');
 const upload = multer({
   dest: 'images',
+  limits: {
+    fileSize: 1000000, // 1 mb
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a Word document'));
+    }
+
+    cb(undefined, true);
+    // cb(new Error('File must be a PDF'));
+    // cb(undefined, true);
+    // cd(undefined, false);
+  },
 });
 
 app.post('/upload', upload.single('upload'), (req, res) => {
