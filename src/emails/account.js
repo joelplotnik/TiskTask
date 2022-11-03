b9307733fdfd9c5eb99df715f515f6ea48799e5c
Joel Plotnik <joelplotnik@gmail.com>
2022-11-03 12:48:38 -0700

    Add sendWelcomeEMail to create user route and remove gitignore files

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,26 +1,26 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-// javascript;
-
 const sgMail = require('@sendgrid/mail');
 
 const SENDGRID_API_KEY =
-  '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';
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  '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';
 
-const msg = {
-  to: 'joelplotnik@gmail.com', // Change to your recipient
-  from: 'joelplotnik@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail
+    .send({
+      to: email,
+      from: 'joelplotnik@gmail.com',
+      subject: 'Weclome to TiskTask!',
+      text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+    })
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent');
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+module.exports = {
+  sendWelcomeEmail,
+};
