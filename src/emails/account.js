dc21aac11d6aa197c5d0eec71212414658b89d2b
Joel Plotnik <joelplotnik@gmail.com>
2022-11-03 14:43:05 -0700

    Create environment variables

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,9 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-const SENDGRID_API_KEY =
-  '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';
-
-sgMail.setApiKey(SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
   sgMail
